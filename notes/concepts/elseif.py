84c27013fa4451742ed12993ae032598a36a8a2e
itasworkshop <itasworkshop@gmail.com>
2023-07-24 17:30:17 +0530

    24 July

---
+++ notes/concepts/elseif.py
@@ -0,0 +1,13 @@
+
+def gradeCalculator(marks):
+    if(marks>=90 and marks<=100):
+        print("A grade")
+    elif(marks>=80 and marks<=90):
+        print("B grade")
+    elif(marks>=70 and marks<=80):
+        print("C grade")
+    else:
+        print("Failed")
+
+
+gradeCalculator(75)
