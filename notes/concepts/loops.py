0264fcee007780bf7a017e7a9f5faa4c57d83e63
itasworkshop <itasworkshop@gmail.com>
2023-07-25 17:25:13 +0530

    25 July

---
+++ notes/concepts/loops.py
@@ -0,0 +1,42 @@
+
+
+"""Loops in python
+
+
+mylist = [1,2,3,4,6,8]
+
+for i in mylist:
+    print(i)
+
+
+for i in range(5):
+    print("Hello World!")
+
+
+for i in range(5):
+    print("Hello World!",i*2)
+    """
+for i in range(1,10,3):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ notes/concepts/loops1.py
@@ -0,0 +1,22 @@
+"""while loop
+
+count = 0
+
+while(count<5):
+    print("Hello World!",count)
+    count = count +1
+
+
+count = 5
+
+while(count>0):
+    print("Hello World!", count)
+    count = count - 1
+    """
+mylist = [1,2,3,4,56,67]
+count = 0
+
+while(count<len(mylist)):
+    print(mylist[count])
+    count = count +1
+
